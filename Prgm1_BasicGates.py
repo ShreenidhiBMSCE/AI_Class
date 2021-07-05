c9a7072463f8c861d69eb2b1873767183adee4f1
ShreenidhiBMSCE <86399902+ShreenidhiBMSCE@users.noreply.github.com>
2021-07-05 22:00:33 +0530

    Add files via upload

---
+++ Prgm1_BasicGates.py
@@ -0,0 +1,20 @@
+﻿def main():
+    a=False
+    b=True
+    print("Not operation of a= ",not(a))
+    print("OR operation of a and b= ",(a or b))
+    print("AND operation of a and b= ", (a and b))
+    print("XOR operation of a and b= ", (a ^ b))
+    print("XNOR operation of a and b= ", not(a ^ b))
+    print("IMPLICATION of a and b= ", imp(a,b))
+    print("BIDIRECTIONAL operation of a and b= ",bidir(a,b))
+
+
+def imp(a,b):
+   return (not(a)) or b
+
+def bidir(a,b):
+    return (imp(a,b) and imp(b,a))
+
+if __name__ == '__main__':
+    main()
